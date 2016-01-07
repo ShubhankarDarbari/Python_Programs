efd346d4f9a04041fe1526d0d2c60931eb76440c
ShubhankarDarbari <shubhankar25@gmail.com>
2016-01-07 20:26:29 +0530

    First program

--- vowels.py
+++ vowels.py
@@ -2,4 +2,4 @@ List1 = ["Allan", "Xavier", "Edward", "Alice", "Martha"]
 name = "Allan"
 for name in List1:
     if name[0] in "AEIOU":
-        print(name + " starts with a vowel.")
+        print(name + " starts with a vowel. ")
